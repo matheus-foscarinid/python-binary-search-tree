7ce54518958621f218f12984af229411a0444726
Matheus Dias <matheus.dias@minhavisita.com.br>
2024-08-27 19:51:11 -0300

    fix: adding recursive approach to count_internal

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -18,29 +18,29 @@ class LinkedBinarySearchTree(BinarySearchTreeADT):
     # special case that checks the root
     # If the tree is empty or it has no children
     if self._root is None or (self._root.left is None and self._root.right is None):
+      return 0
+
+    def __count_internal(self, node: Node) -> int:
+      # if the node is None, return 0
+      if node is None:
         return 0
 
-    left_count = self._count_internal(self._root.left)
-    right_count = self._count_internal(self._root.right)
+      # if the node is a leaf, return 0
+      if node.left is None and node.right is None:
+        return 0
+
+      left_count = __count_internal(node.left)
+      right_count = __count_internal(node.right)
+
+      # if the node is not a leaf, return 1 because it is an internal node
+      # plus the left and right counts
+      return 1 + left_count + right_count
+
+    left_count = __count_internal(_root.left)
+    right_count = __count_internal(self._root.right)
 
     # return the sum of the left and right counts
     return left_count + right_count
-
-  def _count_internal(self, node: Node) -> int:
-    # if the node is None, return 0
-    if node is None:
-      return 0
-
-    # if the node is a leaf, return 0
-    if node.left is None and node.right is None:
-      return 0
-
-    left_count = self._count_internal(node.left)
-    right_count = self._count_internal(node.right)
-
-    # if the node is not a leaf, return 1 because it is an internal node
-    # plus the left and right counts
-    return 1 + left_count + right_count
 
   def degree(self, key: Node) -> int: ...
   def height(self, key: Node) -> int: ...
