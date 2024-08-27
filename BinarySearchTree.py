8c188ca7afcf80c9ac0fef17679d39fb85963ea1
Matheus Dias <matheus.dias@minhavisita.com.br>
2024-08-27 18:11:57 -0300

    feat: adding count_internal method with recursive approach using a private method

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -1,7 +1,40 @@
 from BinarySearchTreeADT import BinarySearchTreeADT
 
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
 class LinkedBinarySearchTree(BinarySearchTreeADT):
-  def count_internal(self) -> int: ...
+  def count_internal(self) -> int:
+    # special case that checks the root
+    # If the tree is empty or it has no children
+    if self._root is None or (self._root.left is None and self._root.right is None):
+        return 0
+
+    left_count = self._count_internal(self._root.left)
+    right_count = self._count_internal(self._root.right)
+
+    # return the sum of the left and right counts
+    return left_count + right_count
+
+  def _count_internal(self, node: object) -> int:
+    # if the node is None, return 0
+    if node is None:
+      return 0
+
+    # if the node is a leaf, return 0
+    if node.left is None and node.right is None:
+      return 0
+
+    left_count = self._count_internal(node.left)
+    right_count = self._count_internal(node.right)
+
+    # if the node is not a leaf, return 1 because it is an internal node
+    # plus the left and right counts
+    return 1 + left_count + right_count
+
   def degree(self, key: object) -> int: ...
   def height(self, key: object) -> int: ...
   def level(self, key: object) -> int: ...
