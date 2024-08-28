f425a18537662a0e02b0718762168e3f9f6d1be0
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2024-08-28 16:57:07 -0300

    feat: adding init to class and changing internal method on count_internal

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -14,13 +14,11 @@ class Node:
     return self.left if other_key < self.key else self.right
 
 class LinkedBinarySearchTree(BinarySearchTreeADT):
+  def __init__(self) -> None:
+    self._root: Node = None
+  
   def count_internal(self) -> int:
-    # special case that checks the root
-    # If the tree is empty or it has no children
-    if self._root is None or (self._root.left is None and self._root.right is None):
-      return 0
-
-    def __count_internal(self, node: Node) -> int:
+    def count_internal(self, node: Node) -> int:
       # if the node is None, return 0
       if node is None:
         return 0
@@ -29,20 +27,26 @@ class LinkedBinarySearchTree(BinarySearchTreeADT):
       if node.left is None and node.right is None:
         return 0
 
-      left_count = __count_internal(node.left)
-      right_count = __count_internal(node.right)
+      left_count = count_internal(node.left)
+      right_count = count_internal(node.right)
 
       # if the node is not a leaf, return 1 because it is an internal node
       # plus the left and right counts
       return 1 + left_count + right_count
 
-    left_count = __count_internal(_root.left)
-    right_count = __count_internal(self._root.right)
+    # special case that checks the root
+    # if the tree is empty or it has no children
+    if self._root is None or (self._root.left is None and self._root.right is None):
+      return 0
+
+    left_count = count_internal(_root.left)
+    right_count = count_internal(self._root.right)
 
     # return the sum of the left and right counts
     return left_count + right_count
 
-  def degree(self, key: Node) -> int: ...
+  def degree(self, key: Node) -> int:
+
   def height(self, key: Node) -> int: ...
   def level(self, key: Node) -> int: ...
   def ancestor(self, key: Node) -> str: ...
