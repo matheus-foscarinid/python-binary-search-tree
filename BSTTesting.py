5e883dcc23fb4cd260a17c1f5ac83001f62f8797
Matheus Dias <matheus.dias@minhavisita.com.br>
2024-09-02 10:21:43 -0300

    chore: adding tests for default methods

--- BSTTesting.py
+++ BSTTesting.py
@@ -24,6 +24,38 @@ class TestBinarySearchTree(unittest.TestCase):
         self.assertEqual(self.bst.search(18), 18)
         self.assertEqual(self.bst.search(100), None)
 
+    def test_delete(self):
+        # leaf
+        self.assertTrue(self.bst.delete(3)) 
+        self.assertIsNone(self.bst.search(3))
+
+        # node with one child
+        self.assertTrue(self.bst.delete(5))
+        self.assertIsNone(self.bst.search(5))
+
+        # node with two children
+        self.assertTrue(self.bst.delete(10))
+        self.assertIsNone(self.bst.search(10))
+
+        # node not found
+        self.assertFalse(self.bst.delete(100))
+
+    def test_clear(self):
+        self.bst.clear()
+        self.assertTrue(self.bst.is_empty())
+
+    def test_is_empty(self):
+        self.assertFalse(self.bst.is_empty())
+        self.bst.clear()
+        self.assertTrue(self.bst.is_empty())
+
+    def test_str(self):
+        # just check if the string representation is not empty 
+        self.assertNotEqual(str(self.bst), '[empty]')
+        self.bst.clear()
+        # after clearing the tree, the string representation should be '[empty]'
+        self.assertEqual(str(self.bst), '[empty]')
+
 
 if __name__ == '__main__':
     unittest.main()
